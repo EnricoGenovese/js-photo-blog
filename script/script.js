d0810dab74410fdcddc5143f46bdcb605d5802d8
EnricoGenovese <enricogeno@libero.it>
2024-11-20 15:34:25 +0100

    add screenshots for part 2; capitalize figure capitions; delete useless function; update milestones in  JS;

--- script/script.js
+++ script/script.js
@@ -1,4 +1,4 @@
-/*
+/* Paete 1
 *Milestone 1*
 Sfruttando gli screen e gli asset in allegato riproduciamo la grafica proposta in maniera statica: utilizzando soltanto HTML e CSS e riproducendo una singola fotografia (usiamo una qualunque immagine a piacimento)
 *Milestone 2*
@@ -11,7 +11,17 @@ Inseriamo un foglio JavaScript ed effettuiamo una chiamata AJAX all’API di JSO
 rendi la pagina responsive, in modo che su mobile e tablet le foto si dispongano man mano una sotto l’altra ed il titolo abbia una dimensione adeguata
 */
 
-
+/* Parte 2
+*CONSEGNA*
+*Milestone 1*
+Facciamo in modo di creare un overlay che copra l’intera pagina e all’interno, centrata, disponiamo un’immagine qualunque ed un button di chiusura.
+*Milestone 2*
+Facciamo sparire l’overlay con l’aiuto di una classe CSS che imposti il display: none .
+Dopodiché facciamo sì che cliccando una qualunque foto. L’overlay ricompaia.
+Cliccando invece il button di chiusura, l’overlay scompare nuovamente.
+*Milestone 3*
+Inseriamo il pezzo di logica finale: quando una foto viene cliccata, dobbiamo fare in modo che sia proprio quella foto a essere mostrata all’interno dell’overlay.
+*/
 const myAlbum = document.getElementById("myAlbum");
 
 const baseURL = "https://jsonplaceholder.typicode.com/";
@@ -36,19 +46,13 @@ axios.get(baseURL + type,
             <img src="${obj.url}" alt="${obj.title} id="${obj.id}">
             <figcaption class="caption">${obj.title}</figcaption>
         </figure> `
-
     })
-    // const caption = document.querySelectorAll(".caption")
-    // console.log(caption)
-    // capitalizeAll(caption.innerText)
     myAlbum.innerHTML = template;
     photoEvent(myAlbum)
 }).catch((error) => {
     console.log(error);
 });
 
-
-// const photoCaption = document.querySelectorAll("caption");
 const overlay = document.getElementById("overlay");
 const close = document.querySelector("#overlay button")
 
@@ -57,21 +61,6 @@ const close = document.querySelector("#overlay button")
 close.addEventListener("click", function () {
     overlay.classList.add("d-none");
 })
-
-
-// function capitalizeAll(arr) {
-//     const strings = arr.map((word) => word.split());
-//     console.log(strings)
-//     const firstCapital = strings.map((string => string.map((word) => word.charAt(0).toUpperCase() + word.slice(1))));
-//     console.log(firstCapital);
-//     const capitalized = firstCapital.map((string) => string.join(" "));
-//     console.log(capitalized);
-//     return capitalized;
-// }
-
-// const myArray = ["cane gatto scimmia", "rosso blu verde giallo", "banana ciliegia arancia mela", "sedia tavolo armadio, porta"];
-
-// capitalizeAll(myArray);
 
 
 function photoEvent() {
